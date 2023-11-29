d5af8afd52feabf418a6d9915bd387ef7f55084d
Caster <daniel.dabrowski.177@gmail.com>
2023-11-29 16:37:52 +0100

    feat: update home component to use getPosts method for displaying posts

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -1,10 +1,17 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { PostService } from '../../services/post.service';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
-  styleUrls: ['./home.component.scss']
+  styleUrls: ['./home.component.scss'],
 })
-export class HomeComponent {
+export class HomeComponent implements OnInit {
+  posts: any[] = [];
 
+  constructor(private postService: PostService) {}
+
+  ngOnInit() {
+    this.posts = this.postService.getPosts();
+  }
 }
