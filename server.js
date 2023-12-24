4521fcdfbe20b2eed5f0c46ec7f69da9321d47e7
Caster <daniel.dabrowski.177@gmail.com>
2023-12-24 21:44:02 +0100

    chore: prepare for production

--- server.js
+++ server.js
@@ -11,10 +11,10 @@ app.use(cors());
 dotenv.config();
 app.use(express.json());
 
-app.use(express.static(path.join(__dirname, "/build")));
+app.use(express.static(path.join(__dirname, "/caster-blog-app/build")));
 
 app.get("*", (req, res) => {
-  res.sendFile(path.join(__dirname, "/build", "index.html"));
+  res.sendFile(path.join(__dirname, "/caster-blog-app/build", "index.html"));
 });
 
 mongoose.connect(process.env.MONGO_URL);
