456719c86d3608f0055836e20e470bb2c887726a
Caster <daniel.dabrowski.177@gmail.com>
2023-12-25 20:59:40 +0100

    chore: prepare for production

--- server.js
+++ server.js
@@ -11,12 +11,18 @@ app.use(cors());
 dotenv.config();
 app.use(express.json());
 
+app.use(express.static(path.join(__dirname, "/dist/caster-blog-app")));
+
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "/dist/caster-blog-app", "index.html"));
+});
+
 mongoose.connect(process.env.MONGO_URL);
 
 app.use("/api/posts", postsRouter);
 
 app.listen(process.env.PORT || 3000, () => {
   console.log(
-    `Server works at https://caster-angular-blog-app-api.onrender.com`
+    `Server works at https://caster-angular-blog-app-full.onrender.com`
   );
 });
